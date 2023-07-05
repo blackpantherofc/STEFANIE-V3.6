b7cd5f9628c1a41e0bdf2ee855e8e430214939bf
White Shadow Ofc <88616416+whiteshadowofficial@users.noreply.github.com>
2023-07-05 17:55:06 +0530

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,16 +1,49 @@
-const chalk = require("chalk")
-const fs = require("fs")
+//════════════════════════════//
+//If you want to recode, reupload
+//or copy the codes/script,
+//pls give credit
+//no credit? i will take action immediately
+//© 2022 Xeon Bot Inc. Cheems Bot MD
+//Thank you to Lord Buddha, Family and Myself
+//════════════════════════════//
+//recode kar ke youtube pe upload kar rhe hai ya
+//codes copy kar ke apne script me dal rhe
+//hai to, description me xeon ka yt channel
+// ka link paste kr dena as a cradit or github 
+//repo me bhi tag kardena baki jo
+//bhi karna hai apki marzi, thank you!🦄
+//════════════════════════════//
+//If you recode and uploading on your channel
+//or copy pasting the codes in ur script, 
+//i give permission to do as long as you
+//put Xeons youtube channel link in the video
+//description and tag me on githuh repo, 
+//thank you🦄
+//════════════════════════════//
 
-//aumto presence update
+const fs = require('fs')
+const chalk = require('chalk')
+
+//Api Website\\
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+//Api Key\\
+global.APIKeys = {
+	'https://zenzapis.xyz': 'your key',
+}
+
+
+//aumto functioner
 global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
+global.autoReadGc = false //auto reading in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
-global.autoblockmorroco = false //auto block 212 (true to on, false to off)
-global.autokickmorroco = false //auto kick 212 (true to on, false to off) 
-global.antispam = false //auto kick spammer (true to on, false to off)
+global.available = true //auto available (true to on, false to off)
 
-
-//if api key expire, u can generate one from here: https://beta.openai.com/account/api-keys
-global.keyopenai = "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"
+//semt menu variants
+global.vidmenu = fs.readFileSync("./MyGallery/theme/stefanie_bot.mp4") //gif and video menu
 
 //documents variants
 global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
@@ -21,49 +54,90 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94779529221'] //ur owner number
-global.ownernomer = "94779529221" //ur owner number2
-global.ownername = "@Stefanie Whatsapp bot" //ur owner name
-global.ytname = "Yt: WSMODZ OFC" //ur yt chanel name
-global.socialm = "GitHub: White Shadow" //ur github or insta name
+global.vcardowner = ['94779529221'] //ur owner number
+global.ownername = "Mr.White Shadow" //ur owner name
+global.ytname = "YT: Xeon" //ur yt chanel name
+global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
-//new
-global.botname = "Stefanie Bot Md V3.6"
-global.ownernumber = '94779529221'
-global.ownername = 'Mr.White Shadow'
-global.ownerNumber = ["94779529221@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@WSMODSOFC"
-global.websitex = "hhttps://youtube.com/@WSMODSOFC"
-global.wagc = "https://chat.whatsapp.com/BNd0LOopWWN8vryB9QZbOk"
-global.themeemoji = '🧨'
-global.wm = "Stefanie Bot Inc."
+//bot bomdy 
+global.owner = ['94779529221']
+global.ownertag = '94779529221' //ur tag number
+global.botname = 'Stefanie Whatsapp bot' //ur bot name
+global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://github.com/whiteshadowofficial/STEFANIE-V3.6' //script link
-global.packname = "Sticker By"
-global.author = "Mr.WhiteShadow\n\n+94779529221"
-global.creator = "94779529221@s.whatsapp.net"
-global.prefa = ['','!','.','#','&']
-global.sessionName = 'session'
-global.hituet = 0
+global.reactmoji = "😘" //ur menu react emoji
+global.themeemoji = "🦄" //ur theme emoji
+global.packname = "Stefanie Whatsapp bot" //ur sticker watermark packname
+global.author = "Stefanie Whatsapp bot" //ur sticker watermark author
 
-//media target
+//Bot theme media
 global.thum = fs.readFileSync("./MyGallery/theme/Stefanie.png") //ur thumb pic
 global.log0 = fs.readFileSync("./MyGallery/theme/Stefanie.png") //ur logo pic
 global.err4r = fs.readFileSync("./MyGallery/theme/Stefanie.png") //ur error pic
 global.thumb = fs.readFileSync("./MyGallery/theme/Stefanie.png") //ur thumb pic
-global.defaultpp = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60' //default pp wa
 
-//menu image maker
+//damtabase
+global.premium = ['94779529221'] //ur premium numbers
+global.ntilinkytvid = []
+global.ntilinkytch = []
+global.ntilinkig = []
+global.ntilinkfb = []
+global.ntilinktg = []
+global.ntilinktt = []
+global.ntilinktwt = []
+global.ntilinkall = []
+global.nticall = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.rkyt = []
+global.wlcm = []
+global.gcrevoke = []
+global.autorep = []
+global.ntilink = []
+
+//omther
+global.sessionName = 'session'
+global.antitags = true
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: 'Done ✓',
+    admin: 'This Feature Is Only For Admin!',
+    botAdmin: 'Bot Must Be Admin First!',
+    owner: 'This Feature Is Only For Owner!',
+    group: 'Feature Used Only For Groups!',
+    private: 'Features Used Only For Private Chat!',
+    bot: 'This Feature Is Only For Bot',
+    wait: 'In process...',
+    linkm: 'Where is the link?',
+    error: 'Error!!',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+    ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
+    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
+    banChat: 'The bot was banned in this group, please contact the owner to unban'
+}
+    global.limitawal = {
+    premium: "Infinity",
+    free: 12,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
+}
+global.thumb = fs.readFileSync('./MyGallery/theme/Stefanie.png')
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-
-//messages
-global.mess = {
-    success: 'Here you go!', 
-}
-
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
 	fs.unwatchFile(file)
